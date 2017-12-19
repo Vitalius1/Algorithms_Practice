0338c0a170f774c6faf24e5c06802ca9dedaf4fe
VitalieBraga <vitaliebraga@gmail.com>
2017-12-18 20:04:22 -0800

    add comments

--- LinkedLists.js
+++ LinkedLists.js
@@ -1,24 +1,27 @@
+// Function constructor for Singley Linked List
 function Sll(){
     this.head = null;
 }
+// Function constructor for Node in SLL
 function Node(val){
     this.value = val;
     this.next = null;
 }
 
+// Prototype function of Sll of adding a new node
 Sll.prototype.add = function(val) {
     myNewNode = new Node(val);
-    if (!this.head){
-        this.head = myNewNode;
+    if (!this.head){ // check if we don't have a Singley Linked List started -->
+        this.head = myNewNode; // --> start the SLL at the new node
     } else{
-        var runner = this.head;
+        var runner = this.head; // else traverse the list with a runner to find the last node in SLL
         while (runner.next){
             runner = runner.next;
         }
-        runner.next = myNewNode;
+        runner.next = myNewNode; // assign to .next of the last node the newly created node
     }
-    return this;
-}
+    return this; // return Sll to be able to chain methods
+};
 
 var mySll = new Sll();
 mySll.add(20).add(50);
