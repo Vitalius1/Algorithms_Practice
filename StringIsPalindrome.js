1ca08177ca6693a9c3775f55fd553bac5795e9e4
VitalieBraga <vitaliebraga@gmail.com>
2017-12-31 17:12:37 -0800

    added a function to check if a string is a palindrome removing first all non-word characters

--- StringIsPalindrome.js
+++ StringIsPalindrome.js
@@ -6,8 +6,22 @@
 
 // ++++++ Using split(), reverse(), join() +++++
 function isPalindrome_1(string) {
-    let strReverse = string.split('').reverse().join('');
+    let strReverse = string.toLowerCase().split('').reverse().join('');
     return string === strReverse;
 }
 
-console.log(isPalindrome_1("aha"));
+// ++++++ Using replace() to remove the non-word characters plus the underscore ++++++
+function isPalindrome_2(string) {
+    string = string.toLowerCase().replace(/[\W_]/g, '');
+    let lastIdx = string.length - 1,
+        i = 0;
+    for (; i < lastIdx / 2; i++) {
+        if (string[i] !== string[lastIdx - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPalindrome_1("aha")); // => true
+console.log(isPalindrome_2("ah*&^%%_a")); // => true
